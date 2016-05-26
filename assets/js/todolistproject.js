77d2275e81e580a850a35c935a0331bbf394d2bf
Abose Ukhun <bexis18@gmail.com>
2016-05-27 01:10:26 +0300

    initial commit

---
+++ assets/js/todolistproject.js
@@ -0,0 +1,28 @@
+//check off specific todos by clicking 
+
+$("ul").on("click","li",function () {
+    $(this).toggleClass("completed")
+});
+
+//click on x to delete todo
+$("ul").on("click", "span",function(event){
+	$(this).parent().fadeOut(5000, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
+
+//working on the input box
+
+$("input[type='text'").keypress(function(event){
+	if (event.which === 13) {
+		//grabbing new todo text
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span>" + todoText + "</li>")
+	};
+})
+
+$(".fa-plus").click(function(){
+	$("input[type='text'").fadeToggle();
+})
